30eb1cd11a6a3f5f5a2597de7ace37b896300498
Sean Trevor <17sowt@queensu.ca>
2021-09-30 13:38:23 -0400

    Updated animation

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,11 @@ import './../css/style.css'
 import { gsap } from "gsap";
 import { CustomEase } from "gsap/CustomEase";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(DrawSVGPlugin, CustomEase);
+gsap.registerPlugin(CustomEase, DrawSVGPlugin, MorphSVGPlugin);
+MorphSVGPlugin.convertToPath("rect, circle");
+
 var titleTextTimeline = gsap.timeline();
 titleTextTimeline.fromTo("#S-mask-path", {duration:1,drawSVG:"0%"}, {duration:0.4,drawSVG:"100%", ease: "circ.inOutinOut"});
 titleTextTimeline.fromTo("#O-mask-path", {duration:0,drawSVG:"0%"}, {duration:0.4,drawSVG:"100%", ease: "circ.inOutinOut"});
@@ -11,4 +14,6 @@ titleTextTimeline.fromTo("#W-mask-path", {duration:0,drawSVG:"0%"}, {duration:0.
 titleTextTimeline.fromTo("#T1-mask-path", {duration:0,drawSVG:"0%"}, {duration:0.2,drawSVG:"100%", ease: "circ.inOutinOut"});
 titleTextTimeline.fromTo("#T2-mask-path", {duration:0,drawSVG:"0%"}, {duration:0.1,drawSVG:"100%", ease: "circ.inOutinOut"});
 
-titleTextTimeline.fromTo(".blur", {durqation:0, filter:"blur(0px)"}, {duration:2.5, filter:"blur(5px)"});
+titleTextTimeline.fromTo(".blur", {duration:0, filter:"blur(0px)"}, {duration:1.2, filter:"blur(5px)"});
+titleTextTimeline.to(".titlePath", {duration:0, mask:"none"});
+titleTextTimeline.to(".titlePath", {duration: 1, morphSVG:"#centerCircle"});
