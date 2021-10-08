414c430c02f0dbdb1602cddfb2a7a55b09257e8b
Sean Trevor <17sowt@queensu.ca>
2021-10-08 16:57:11 -0400

    parallax update

--- src/js/star-warp.js
+++ src/js/star-warp.js
@@ -3,79 +3,79 @@ import { gsap } from "gsap";
 import { random } from 'gsap/all';
 import { CustomWiggle } from "gsap/CustomWiggle";
 import { CustomEase } from "gsap/CustomEase";
-import { create } from 'combined-stream';
 gsap.registerPlugin(CustomEase, CustomWiggle);
-const app = new PIXI.Application({
-    autoResize: true,
-});
+const app = new PIXI.Application();
 const canvas = document.getElementById("pixiCanvas");
 const starTexture = document.getElementById("star");
 const stars = [];
-const spritesContainer = new PIXI.ParticleContainer(500, {
+var starCount = 0;
+const totalSprites = app.renderer instanceof PIXI.Renderer ? 1000 : 10;
+const spritesContainer1 = new PIXI.ParticleContainer(200, {
     scale: true,
     position: true,
-    rotation: true,
-    uvs: true,
     alpha: true,
+    interactive: true,
 });
-const totalSprites = app.renderer instanceof PIXI.Renderer ? 10000 : 10;
-
+const spritesContainer2 = new PIXI.ParticleContainer(200, {
+    scale: true,
+    position: true,
+    alpha: true,
+    interactive: true,
+});
 var starFlickerWiggle = CustomWiggle.create("", {wiggles:1, type:"easeInOut"}); 
 var starFlickerAlpha = 1;
 
 app.renderer.backgroundAlpha = 0;
+app.view.setAttribute('id', 'stage');
+app.view.setAttribute('class', 'parallaxItem');
+app.view.setAttribute('data-speed', '2');
 canvas.appendChild(app.view);
-app.stage.addChild(spritesContainer);
-var startingWidth = app.view.parentNode.clientWidth;
-var startingHeight = app.view.parentNode.clientHeight;
-
+app.stage.addChild(spritesContainer1);
+app.stage.addChild(spritesContainer2);
 resize();
+
+
 
 for (let i = 0; i < totalSprites; i++) {
     var starX = random(0,app.view.width*3);
     var starY = random(0,app.view.height*3);
-    createStar(starX,starY);    
+    createStar(starX,starY, );    
 }
-
 stars.forEach(element => {
-    randomizeStarFlicker(element);
+    StarFlicker(element);
 });
-
-function randomizeStarFlicker(element){
-    element.alpha >= 0.5 ? starFlickerAlpha = element.alpha*0.5 : starFlickerAlpha = element.alpha*1.5;
-    gsap.to(element, {duration:"random(0.1,3)", alpha:starFlickerAlpha,ease:starFlickerWiggle, onComplete:randomizeStarFlicker, onCompleteParams:[element]});
-
-  }
-
- function createStar(starX, starY) {
+function StarFlicker(element){
+    element.alpha >= 0.5 ? starFlickerAlpha = element.alpha*0.3 : starFlickerAlpha = element.alpha*1.8;
+    gsap.to(element, {duration:"random(0.5,5)", alpha: starFlickerAlpha, ease:starFlickerWiggle, onComplete: StarFlicker, onCompleteParams:[element]});
+}
+function createStar(starX, starY) {
+    starCount++;
     const star = PIXI.Sprite.from(starTexture);
     star.anchor.set(0.5);
-    star.scale.set(random(0.008, 0.07));
+    star.scale.set(random(0.008, 0.05));
     star.alpha = starFlickerAlpha;
-    star.tint= 0xafa337;
+    star.tint= 0xffffff;
     star.x = starX;
     star.y = starY;
     stars.push(star);
-    spritesContainer.addChild(star);
-    console.log('created star');
+    starCount<200 ? spritesContainer1.addChild(star) : spritesContainer2.addChild(star);
  } 
-
-
 function resize() {
     const parent = app.view.parentNode;
     app.renderer.resize(parent.clientWidth, parent.clientHeight);
-    adjustStars(parent.clientWidth,parent.clientHeight);
-
 }
-function adjustStars(width, height) {
-    // startingWidth<=width ? starX = random(startingWidth, width) : starX = startingWidth;
-    // startingHeight<=height ? starY = random(startingHeight, height) : starY = startingHeight;
-    // createStar(starX, starY);
-    // app.render();
-}
-
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
 window.addEventListener('resize', resize);
+
+function parallax(e){
+    this.querySelectorAll('.parallaxItem').forEach(item => {
+        const speed = item.getAttribute('data-speed');
+        const x = ((window.innerWidth - e.pageX*speed)/100);
+        const y = ((window.innerHeight - e.pageY*speed)/100);
+        item.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
+document.addEventListener('mousemove', parallax);
---
+++ src/js/titleAnimation.js
@@ -0,0 +1,24 @@
+import { gsap } from "gsap";
+import { CustomEase } from "gsap/CustomEase";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { CustomWiggle } from "gsap/CustomWiggle";
+gsap.registerPlugin(CustomEase, DrawSVGPlugin, MorphSVGPlugin, CustomWiggle);
+MorphSVGPlugin.convertToPath("rect, circle");
+var titleTextTimeline = gsap.timeline();
+
+titleTextTimeline.fromTo("#S-mask-path", {duration:0.5,drawSVG:"0%"}, {duration:0.3,drawSVG:"100%", ease: "circ.inOutinOut"});
+titleTextTimeline.fromTo("#O-mask-path", {duration:0,drawSVG:"0%"}, {duration:0.3,drawSVG:"100%", ease: "circ.inOutinOut"});
+titleTextTimeline.fromTo("#W-mask-path", {duration:0,drawSVG:"0%"}, {duration:0.2,drawSVG:"100%", ease: "circ.inOutinOut"});
+titleTextTimeline.fromTo("#T1-mask-path", {duration:0,drawSVG:"0%"}, {duration:0.1,drawSVG:"100%", ease: "circ.inOutinOut"});
+titleTextTimeline.fromTo("#T2-mask-path", {duration:0,drawSVG:"0%"}, {duration:0.1,drawSVG:"100%", ease: "circ.inOutinOut"});
+titleTextTimeline.to(".titlePath", {duration:0, mask:"none"});
+titleTextTimeline.to(".titlePath", {delay: 1, duration: 0.6, stagger:-0.05, morphSVG:"#centerCircle",ease: "circ.inOutinOut"});
+titleTextTimeline.fromTo(".blur", {duration:0, filter:"blur(0px)"}, {duration:0.6, filter:"blur(5px)"},"<0.5");
+titleTextTimeline.to("#titleText-animation", {duration:1.2, scaleX: 0, scaleY: 0, ease: "expo.out"},"<-0.2");
+titleTextTimeline.to(".glass", {duration:0, visibility: "hidden"});
+titleTextTimeline.to("#titleText-animation", {duration:0, visibility:"hidden"});
+titleTextTimeline.to(".titlePath", {duration:0, visibility:"hidden"});
+titleTextTimeline.to(".title-mask", {duration:0, visibility:"hidden"});
+titleTextTimeline.to(".animation-container", {duration:0, visibility:"hidden"});
+titleTextTimeline.fromTo(".webContainer", {duration: 0, visibility:"visible", opacity: 0},{duration:1, opacity:1},"<-0.9");
