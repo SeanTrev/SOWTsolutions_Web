f1c9ab41d2eca7886f344ce6d180b1f6c741a877
Sean Trevor <17sowt@queensu.ca>
2021-10-21 12:01:47 -0400

    update

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,19 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+// gsap.from(".titleWrapper", {
+//     scrollTrigger: {
+//       trigger: ".titleWrapper",
+//       pin: true,
+
+//       markers:true,
+//       start: "top",
+//       end: "top 2000px"
+//     },
+//     position:"fixed",
+//   });
+
  document.addEventListener('mousemove', parallax);
  function parallax(e){
      this.querySelectorAll('.parallaxItem').forEach(item => {
